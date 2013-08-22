886b8547bbf2f45feb07b5f74450227275e62627
Torstein <torsteinsandven@gmail.com>
2013-08-22 13:08:19 +0200

    ReqestPasrser update

---
+++ src/transfer/ReqestPasrser.java
@@ -0,0 +1,18 @@
+package transfer;
+
+import java.net.Socket;
+
+public class ReqestPasrser implements Runnable{
+
+	Socket socket; 
+	
+	public ReqestPasrser(Socket socket){
+		this.socket = socket; 
+	}
+	
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
