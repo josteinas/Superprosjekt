aa75435cb2d3c91db0ec0a736a73c9329ffc28d5
haakondi <hakon.dissen@gmail.com>
2013-08-22 11:40:25 +0200

    thread packing

--- src/transfer/Receiver.java
+++ src/transfer/Receiver.java
@@ -15,6 +15,7 @@ public class Receiver {
 			while(true){
 				Socket receiveSocket = serverSocket.accept(); 
 				FileReceiver fileReceiver = new FileReceiver(receiveSocket); 
+				(new Thread(fileReceiver)).start();
 				fileReceiver.run(); 
 			}
 			
