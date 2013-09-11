064281228bcaaa00547504fc31c94b90c5200a6b
haakondi <hakon.dissen@gmail.com>
2013-09-11 21:47:12 +0200

    Endret handler

--- src/transfer/RequestHandler.java
+++ src/transfer/RequestHandler.java
@@ -3,46 +3,37 @@ package transfer;
 import java.net.Socket;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 public class RequestHandler implements Runnable{
 	
 	private static BlockingQueue<Socket> blockingQueue; 
 	
-	public RequestHandler(){
-		if (blockingQueue != null){
-			System.out.println("Trying to create a new blockingQueue");
-		}else {
-			blockingQueue = new ArrayBlockingQueue<>(100); 
-		}
+	public RequestHandler(BlockingQueue<Socket> blockingQueue){
+		this.blockingQueue = blockingQueue;
 	}
 
 	@Override
 	public void run() {
 		while(true){
-			if(blockingQueue.isEmpty()){
-				try {
-					wait(50);
-				} catch (InterruptedException e) {
-					System.out.println("InterruptedException in RequestHandler ");
-				} 
-			}else {
+			try {
+				Socket client = blockingQueue.take();
 				//TODO
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
 		}
 	}
 	
-	public static BlockingQueue<Socket> getBlockingQueue(){
-		if(blockingQueue == null){
-			(new Thread(new RequestHandler())).start(); 
-		}
-		return blockingQueue;
-	}
+
 	
-	public static void addSocket(Socket socket){
-		if(blockingQueue == null){
-			(new Thread(new RequestHandler())).start(); 
+	public synchronized static void addSocket(Socket socket){
+		try {
+			while(!blockingQueue.offer(socket, 100, TimeUnit.MILLISECONDS));
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
-		blockingQueue.add(socket);
 	}
 
 }
